25e7256ef14703643a4cdc0eca7a1498242cdeaa
YordanSV <Pitiguirra10_>
2024-06-05 16:30:17 -0600

    prueba

--- src/components/InfoSection.js
+++ src/components/InfoSection.js
@@ -6,7 +6,7 @@ const InfoSection = () => {
       <h2>Nuestro catálogo de videojuegos</h2>
       <h3>Descubre lo que RentGamer tiene para ti</h3>
       <p>
-        En RentGamer nos esforzamos por ser el mejor servicio de alquiler de videojuegos en Ciudad de México. 
+        En RentGamer nos esforzamos por ser el mejor servicio de alquiler de videojuegos en todo el país. 
         Ofrecemos una amplia selección de productos a precios accesibles para que puedas disfrutar de tus juegos 
         favoritos sin límites. Visítanos en línea para conocer más sobre nuestras opciones de alquiler.
       </p>
