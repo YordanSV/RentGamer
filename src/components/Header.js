ba15f9d49e5a218db13fe8de76cef79eb1ed1309
YordanSV <Pitiguirra10_>
2024-06-05 18:05:58 -0600

    actualizacion

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,8 @@ import Nav from './Nav'
 export default function Header() {
   return (
     <header className='header'>
-        <Logo />
+        RentGamer
+        {/* <Logo /> */}
         <Nav />
     </header>
   )
