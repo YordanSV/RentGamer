2f554400bf7f7743fd707a303a35b1dcc2f6e409
YordanSV <Pitiguirra10_>
2024-06-18 18:15:33 -0600

    adaptando a movil

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
 import React from 'react'
+import Logo from './Logo'
 import Nav from './Nav'
 
 export default function Header() {
   return (
     <header className='header'>
-        RentGamer
-        {/* <Logo /> */}
+        <Logo />
         <Nav />
     </header>
   )
