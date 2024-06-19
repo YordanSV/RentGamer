fa65205fec6c4a033c7cb1edf3420ebb5ca9bb4e
YordanSV <Pitiguirra10_>
2024-06-18 21:05:36 -0600

    .

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Logo() {
   return (
     <a href="/" className="logo-link">
-      <img src="/logo.png" alt="#" className="logo" />
+      <img src="/logo.png" alt="RentGamer" className="logo" />
     </a>
   )
 }
