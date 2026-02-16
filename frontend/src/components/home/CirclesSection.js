fda1a94f87bb30038b63fbcdfea5f0d790fed646
Yordan <yordan10_pv@hotmail.com>
2026-02-16 15:27:35 -0600

    fix: elimina imports no usados (useState, useGames) en CirclesSection.js para evitar advertencias ESLint en CI

--- frontend/src/components/home/CirclesSection.js
+++ frontend/src/components/home/CirclesSection.js
@@ -1,7 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
-import { useGames } from '../../contexts/GamesContext';
 
 // Define styled components
 const Section = styled.section`
